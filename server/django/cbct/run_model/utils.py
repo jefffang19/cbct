4af6849f693cf0a769a90b74eba3f61d33b5a684
jefffang19 <jefffang07@gmail.com>
2021-06-09 16:35:56 +0800

    Now uploaded image will run model and show result

--- server/django/cbct/run_model/utils.py
+++ server/django/cbct/run_model/utils.py
@@ -1,5 +1,7 @@
+
+
 def handle_uploaded_file(f):
-    with open('run_model/upload_files/upload.txt', 'wb+') as destination:
+    with open('run_model/upload_files/upload.dcm', 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
 
--- server/django/cbct/run_model/views.py
+++ server/django/cbct/run_model/views.py
@@ -18,6 +18,14 @@ def run_model(request):
 def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
+
+        # check if file format is correct
+        filename = str(request.FILES['file'])
+        print(filename)
+        if filename.split('.')[-1] != 'dcm':
+            print('Wrong Data format')
+            return HttpResponse('Wrong Data format, please upload a .dcm file')
+
         if form.is_valid():
             handle_uploaded_file(request.FILES['file'])
             return HttpResponseRedirect('loading')
@@ -36,4 +44,17 @@ def home(request):
 
 
 def loading(request):
-    return render(request, 'run_model/loading.html')
+    from shutil import copyfile
+    import subprocess
+
+    # copy the uploaded dicom file to model input folder
+    copyfile('run_model/upload_files/upload.dcm', '../../input/in.dcm')
+
+    # run model
+    subprocess.run(['python', '../../inference.py'])
+
+    # copy the result back to server static folder
+    copyfile('../../output/ori.jpg', 'run_model/static/run_model/ori.jpg')
+    copyfile('../../output/proc.jpg', 'run_model/static/run_model/proc.jpg')
+
+    return render(request, 'run_model/show_result.html')
