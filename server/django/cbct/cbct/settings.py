b7880d23c8ec2dc0752bdf4bd815b5f4328b6a6b
jefffang19 <jefffang07@gmail.com>
2021-06-09 17:49:52 +0800

    Add allow host

--- server/django/cbct/cbct/settings.py
+++ server/django/cbct/cbct/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
