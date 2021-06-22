Fix hu scaling bug

server/utils/load_dicom.py
@@ -10,14 +10,36 @@ def read_dicom(path):
     g = glob.glob(os.path.join(path, '*'))
     slices = [pydicom.read_file(s) for s in g]
 
+    # see dicom information
+    # print(slices[0])
+
+    slope = slices[0].RescaleSlope
+    intercept = slices[0].RescaleIntercept
+
+    # get pixel
+    slices = [get_pixel(s) for s in slices]
+
+    # scale hu value
+    slices = [scale_hu(s, slope, intercept) for s in slices]
+    # print(np.min(slices[0]), np.max(slices[0]))
+
     # adjust hu value
     slices = [hu_window(s, window_level=0, window_width=1000) for s in slices]
 
     return slices
 
 
-def hu_window(scan, window_level=40, window_width=80, show_hist=False):
+def get_pixel(scan):
     scan = scan.pixel_array.copy()
+    return scan
+
+
+def scale_hu(scan, slope, intercept):
+    # scan = scan, (scan.min(), scan.max()), (-1000, 1000))
+    return scan * slope + intercept
+
+
+def hu_window(scan, window_level=40, window_width=80, show_hist=False):
     window = [window_level-window_width/2, window_width/2-window_level]
 
     scan = np.where(scan < window[0], window[0], scan)
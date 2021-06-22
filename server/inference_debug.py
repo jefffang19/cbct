7fe733b4a34e7018d6c9d5eda5899c5dcd568189
jefffang19 <jefffang07@gmail.com>
2021-06-22 09:43:58 +0800

    Fix hu scaling bug

---
+++ server/inference_debug.py
@@ -0,0 +1,39 @@
+import model.model as model
+import model.fit as fit
+import utils.load_dicom as load_dicom
+import numpy as np
+from cv2 import cv2
+import os
+
+
+def inference(path):
+
+    # define window level and window width
+    WL = 0
+    WW = 1000
+
+    # init model
+    net = model.get_model(os.path.join(
+        path, 'weights/save_model_unet_resnet152.pth'))
+
+    # read dicom img
+    slices = load_dicom.read_dicom(os.path.join(path, 'input'))
+
+    # fit model
+    pred = fit.fit(net, slices[0])
+
+    # save the results to output/
+    s = ((slices[0] + WW/2) / WW * 255).astype(np.uint8)
+    pr = ((pred - np.min(pred)) / (np.max(pred) -
+                                   np.min(pred)) * 255).astype(np.uint8)
+
+    cv2.imwrite(os.path.join(path, 'output/ori.jpg'), s)
+    cv2.imwrite(os.path.join(path, 'output/proc.jpg'), pr)
+
+    print('result saved!')
+
+
+if __name__ == '__main__':
+    path = '.'
+
+    inference(path)
--- server/utils/load_dicom.py
+++ server/utils/load_dicom.py
@@ -10,14 +10,36 @@ def read_dicom(path):
     g = glob.glob(os.path.join(path, '*'))
     slices = [pydicom.read_file(s) for s in g]
 
+    # see dicom information
+    # print(slices[0])
+
+    slope = slices[0].RescaleSlope
+    intercept = slices[0].RescaleIntercept
+
+    # get pixel
+    slices = [get_pixel(s) for s in slices]
+
+    # scale hu value
+    slices = [scale_hu(s, slope, intercept) for s in slices]
+    # print(np.min(slices[0]), np.max(slices[0]))
+
     # adjust hu value
     slices = [hu_window(s, window_level=0, window_width=1000) for s in slices]
 
     return slices
 
 
-def hu_window(scan, window_level=40, window_width=80, show_hist=False):
+def get_pixel(scan):
     scan = scan.pixel_array.copy()
+    return scan
+
+
+def scale_hu(scan, slope, intercept):
+    # scan = scan, (scan.min(), scan.max()), (-1000, 1000))
+    return scan * slope + intercept
+
+
+def hu_window(scan, window_level=40, window_width=80, show_hist=False):
     window = [window_level-window_width/2, window_width/2-window_level]
 
     scan = np.where(scan < window[0], window[0], scan)
